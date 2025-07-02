faf29a8d9921219d70e20cbbaa47045ddb8f0092
ÖZKAN DALGIÇ <ozkan.dalgic@lymranet.com>
2025-07-02 15:36:35 +0300

    DB update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -56,7 +56,7 @@ public partial class ApplicationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=213.159.29.100;Database=TourManagementDb;User Id=tourlogin;Password=d93cHL:?bHb[.a%/4c3:yGALR{KGBAdsa;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=213.159.29.102,14333;Database=TourManagementDb;Persist Security Info=True;User ID=webSa;Password=d93.cHL:?bHb[.a%/4c3:yGALR{KGBAdsa;MultipleActiveResultSets=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Migrations/20250702122644_AddYourChangesMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TourManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYourChangesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "B2BAgencyId",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "B2BAgencyId",
+                table: "Activities");
+        }
+    }
+}
